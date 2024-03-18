34ac01fbfd421e021ab5f746fc1d1a590e0300ea
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 11:46:33 -0700

    Added Day1/main.js

---
+++ Day1/main.js
@@ -0,0 +1,16 @@
+// Question NO 2
+let name1 = "Shehroz";
+console.log(`Hello ${name1} would you like to learn some TypeScript today?`);
+// Question No 3
+let name2 = "M.Hanif";
+// For UpperCase
+let bigLetter = name2.toUpperCase();
+console.log(`${bigLetter}`);
+// For lowercase()
+let smallLetter = name2.toLowerCase();
+console.log(`${smallLetter}`);
+//For TitleCase
+let firstLetter = name2.charAt(0).toUpperCase();
+let secondLetter = name2.slice(1).toLowerCase();
+console.log(`${firstLetter + secondLetter}`);
+export {};
