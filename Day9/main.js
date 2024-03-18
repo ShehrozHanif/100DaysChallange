2d82493b97fd20fbc3054eb111040ac2e7fc5da4
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:19:53 -0700

    Added Day9/main.js

---
+++ Day9/main.js
@@ -0,0 +1,64 @@
+"use strict";
+// Day 9
+//  //    //                   (Question No 25)
+let alienColor = "green";
+if (alienColor == "green") {
+    console.log("The player just earned 5 points");
+}
+// its Pass version
+alienColor = "yellow";
+if (alienColor == "green") {
+    console.log("its fail version");
+}
+//  its fail version
+//  //            //         (Question No 26)
+let alien_colour = "green";
+if (alien_colour == "green") {
+    console.log("The player just earned 5 points fir shooting the Alien");
+}
+else {
+    console.log("You just earn 10 Points");
+}
+// Version That runs if Statement
+let alien_Color = "yellow";
+if (alien_Color == "green") {
+    console.log("The player just earned 5 points fir shooting the Alien");
+}
+else {
+    console.log("You just earn 10 Points");
+}
+// Version that rin else statement
+// // //    //                (Question NO 27)
+// When the version is green
+let alianColor = "green";
+if (alianColor == "green") {
+    console.log("The player just earned 5 points fir shooting the Alien");
+}
+else if (alianColor == "yellow") {
+    console.log("The player just earned 10 points fir shooting the Alien");
+}
+else if (alianColor == "red") {
+    console.log("The player just earned 15 points fir shooting the Alien");
+}
+// when the version is yellow
+alianColor = "yellow";
+if (alianColor == "green") {
+    console.log("The player just earned 5 points fir shooting the Alien");
+}
+else if (alianColor == "yellow") {
+    console.log("The player just earned 10 points fir shooting the Alien");
+}
+else if (alianColor == "red") {
+    console.log("The player just earned 15 points fir shooting the Alien");
+}
+// When the version is red
+alianColor = "red";
+if (alianColor == "green") {
+    console.log("The player just earned 5 points fir shooting the Alien");
+}
+else if (alianColor == "yellow") {
+    console.log("The player just earned 10 points fir shooting the Alien");
+}
+else if (alianColor == "red") {
+    console.log("The player just earned 15 points fir shooting the Alien");
+}
