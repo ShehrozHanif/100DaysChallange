08624d3541c4253e03b9011aecca22682dd48acc
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:25:52 -0700

    Added Day1/main.ts

--- Day2/main1.ts
+++ Day2/main1.ts
@@ -1,8 +1,8 @@
 // Question No 1
 
-// let author = "Imran Khan"
-// let quotation ="You only loose when you give up"
-// console.log(`"${author} once said,"${quotation}""`)
+let author = "Imran Khan"
+let quotation ="You only loose when you give up"
+console.log(`"${author} once said,"${quotation}""`)
 
 // Question No 2
 
@@ -10,11 +10,11 @@
 
 // Question No 3
 
-//With Extra Space and NewLine
-// let name1 = "\t\n Shehroz \t\n"
-// console.log(name1);
+// With Extra Space and NewLine
+let name1 = "\t\n Shehroz \t\n"
+console.log(name1);
 
 // // Now remove the Extra Spaces 
-// console.log(`${name1.trim()}`)
+console.log(`${name1.trim()}`)
 
 
