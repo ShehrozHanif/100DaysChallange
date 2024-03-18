56fe1b43d2f527c09b4bf690a7271c9df3eb20f9
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:13:30 -0700

    Added Day5/main.js

---
+++ Day5/main.js
@@ -0,0 +1,16 @@
+// Day 5
+//  Question No 13
+let transports = ["Honda Civic", "Tesla Car", "Ferrari Car", "Rolls Royce"];
+for (let i = 0; i < transports.length; i++) {
+    let pre = "I would like to own a";
+    console.log(`${pre} ${transports[i]}`);
+}
+// What we did in this program just used iteration to print in a Sequence
+// Question No 14
+let guestList = ["Imran Khan Sahab", "Tariq Jameel Sahab", "Sir Zia"];
+for (let i = 0; i < guestList.length; i++) {
+    let invitation = "I would like you to invite on Dinner";
+    console.log(guestList[i] + " " + invitation);
+}
+export {};
+// What we did in this code/Program just follow the basic of iteration
