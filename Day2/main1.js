20c99bba58cf77cc0ddc3b44bd56a7d058c0717f
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:25:39 -0700

    Added Day2/main1.js

--- Day2/main1.js
+++ Day2/main1.js
@@ -1,14 +1,14 @@
 "use strict";
 // Question No 1
 Object.defineProperty(exports, "__esModule", { value: true });
-// let author = "Imran Khan"
-// let quotation ="You only loose when you give up"
-// console.log(`"${author} once said,"${quotation}""`)
+let author = "Imran Khan";
+let quotation = "You only loose when you give up";
+console.log(`"${author} once said,"${quotation}""`);
 // Question No 2
 // I already did in Question no 1 in short both answer in Q1
 // Question No 3
-//With Extra Space and NewLine
-// let name1 = "\t\n Shehroz \t\n"
-// console.log(name1);
+// With Extra Space and NewLine
+let name1 = "\t\n Shehroz \t\n";
+console.log(name1);
 // // Now remove the Extra Spaces 
-// console.log(`${name1.trim()}`)
+console.log(`${name1.trim()}`);
