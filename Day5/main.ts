2b503814f2df9c46e204831eccbdc68b0284fad0
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:14:04 -0700

    Added Day5/main.ts

---
+++ Day5/main.ts
@@ -0,0 +1,19 @@
+// Day 5
+//  Question No 13
+
+let transports :string[]= ["Honda Civic", "Tesla Car", "Ferrari Car","Rolls Royce"]
+for(let i =0; i<transports.length ; i++  ){
+    let pre:string ="I would like to own a" 
+    console.log(`${pre} ${transports[i]}`)
+}
+// What we did in this program just used iteration to print in a Sequence
+
+// Question No 14
+
+let guestList:String[] = ["Imran Khan Sahab","Tariq Jameel Sahab","Sir Zia"]
+for(let i = 0; i<guestList.length; i++){
+    let invitation:string ="I would like you to invite on Dinner"
+    console.log(guestList[i] + " "+ invitation);
+}
+
+// What we did in this code/Program just follow the basic of iteration
