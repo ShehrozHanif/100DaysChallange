07d6bf49ccab2c8707dfb0ada5b5e6fa090dbeba
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:10:08 -0700

    Added Day3/main2.js

---
+++ Day3/main2.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Day 3
+// Question No 7
+let add1 = 5;
+let add2 = 3;
+console.log(add1 + add2);
+let sub1 = 2;
+let sub2 = 10;
+console.log(sub2 - sub1);
+let mul1 = 2;
+let mul2 = 4;
+console.log(mul1 * mul2);
+let div1 = 2;
+let div2 = 16;
+console.log(div2 / div1);
+// Question NO 8
+// I alreay give answer of this Q8 in Q7, along the answer With Q7
+// Question No 9
+let favouriteNymber = 6;
+console.log(`My favourite number is ${favouriteNymber}`);
