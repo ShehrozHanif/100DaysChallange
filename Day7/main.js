bdac1982670898aa04aa8ebf167faff01922b91d
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:17:47 -0700

    Added Day7/main.js

---
+++ Day7/main.js
@@ -0,0 +1,25 @@
+"use strict";
+// Day 7
+//  //                               Questiion No 19
+let guestList = ["Mrs Imran khan", "Imran Khan Sahab", "Murad Saaed", "Tariq Jameel Sahab", "Sir Zia", "Yateem Bacho"];
+// Guest List
+let number = guestList.length;
+// Now we find how many guest arrive and save the value in variable called number
+console.log(`we are inviting ${number} people for dinner`);
+// Now we print how many people we Invite for Dinner
+//  //                               Question No 20
+let impLang = ["German", "French", "Italian", "Arabic", "English", "Chinese"];
+// we store some values in an Array
+for (let i = 0; i < impLang.length; i++) {
+    console.log(impLang[i]);
+}
+// Now print this with the help of console
+//  //      //                       Question No 21
+let user = {
+    fName: "Shehroz",
+    lName: "M.Hanif",
+    age: 26
+};
+// As you can see we make an onject called User
+console.log(`My name is ${user.fName} and my Sir name ${user.lName} is and my age is ${user.age}`);
+//  As ususal we print this through Console
