05b3ecd36ad80b22eb5aaf69c0f138afd2d7ef2f
Shehroz Hnaif <Shehrozhanif54@gmail.com>
2024-03-18 12:10:19 -0700

    Added Day3/main2.ts

---
+++ Day3/main2.ts
@@ -0,0 +1,26 @@
+// Day 3
+// Question No 7
+let add1:number = 5;
+let add2:number = 3;
+console.log(add1 + add2)
+
+let sub1:number =   2;
+let sub2:number =  10;
+console.log(sub2 - sub1);
+
+let mul1:number = 2;
+let mul2:number = 4;
+console.log(mul1 * mul2);
+
+let div1:number = 2;
+let div2:number = 16;
+console.log(div2/div1);
+
+// Question NO 8
+// I alreay give answer of this Q8 in Q7, along the answer With Q7
+
+// Question No 9
+
+let favouriteNymber:number = 6;
+console.log(`My favourite number is ${favouriteNymber}`) 
+
