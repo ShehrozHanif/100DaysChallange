d74c2e9db9af9347f0dd049982ae1069edf9a07b
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-06-10 16:17:54 -0700

    update

---
+++ Day54/index.js
@@ -0,0 +1,4 @@
+alert("Hello world")
+
+// we print hello world in sand box.
+//copy the path of index.html and paste it in the browser url.
---
+++ Day54/variable.js
@@ -0,0 +1,26 @@
+// Few task regarding variables
+
+
+/*
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+
+let admin;
+let name= "john"
+
+ admin = name
+ alert(admin)
+
+
+
+
+// Second task
+/*.Create a variable with the name of our planet. How would you name such a variable?
+  .Create a variable to store the name of a current visitor to a website. How would you name that variable?
+*/
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
